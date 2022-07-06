96655c8977e3c6dc8b7a5f258fb9575aa97d3fa2
pbarkley_desk <rpbarkley99@gmail.com>
2022-07-06 11:21:15 -0500

    add submit button functionality and api call

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 var availableTags = [];  
+var apiKey = "b9705267b2365b2bbc620b6ed43e5bcf"
 
 $( function() { 
     $("#inputCity").autocomplete({
@@ -7,3 +8,14 @@ $( function() {
 });
 
 $("inputCity").push(availableTags);
+
+function onSubmit(event) {
+  event.preventDefault()
+  var userInput = $("#inputCity").val()
+  console.log(userInput)
+  fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&appid=" + apiKey)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+$(".submitBtn").click(onSubmit)
