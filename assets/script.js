eb5a65b23276b4b9fbde41624e2667123bf7d336
pbarkley_desk <rpbarkley99@gmail.com>
2022-07-06 19:41:03 -0500

    successfully parse data from fetch

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,62 @@
 var availableTags = [];  
+
 var apiKey = "b9705267b2365b2bbc620b6ed43e5bcf"
 
-$( function() { 
-    $("#inputCity").autocomplete({
-      source: availableTags
-    });
-});
+var latitude 
+var longitude
+var loc
+var temperature
+var humidity
+var windspeed
+var uvIndex
 
-$("inputCity").push(availableTags);
+
+// $( function() { 
+//     $("#inputCity").autocomplete({
+//       source: availableTags
+//     });
+// });
 
 function onSubmit(event) {
   event.preventDefault()
-  var userInput = $("#inputCity").val()
+
+  // $("inputCity").push(availableTags);
+  userInput = $("#inputCity").val()
   console.log(userInput)
+
   fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&appid=" + apiKey)
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+      loc = data[0].name
+      latitude = data[0].lat
+      longitude = data[0].lon
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=imperial")
+          .then(function (response) {
+            return response.json();
+         })
+          .then(function (data) {
+            console.log(data);
+            temperature = data.current.temp
+            humidity = data.current.humidity
+            windspeed = data.current.wind_speed
+            uvIndex = data.current.uvi
+            console.log(temperature)
+            console.log(humidity)
+            console.log(windspeed)
+            console.log(uvIndex)
+            console.log(loc)
+            $(".location").text(loc)
+            
+          })
+    })
+  
+  
+  
+  
+  
+}         
 
 $(".submitBtn").click(onSubmit)
